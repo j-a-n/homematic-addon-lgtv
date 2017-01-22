be70e3ff9cf7a3db5f18e76fb900f35de489810f
Jan Schneider <j.schneider@aktivoptik.de>
2017-01-22 01:55:27 +0100

    Add cmdline script

--- addon/lib/lgtv.tcl
+++ addon/lib/lgtv.tcl
@@ -15,7 +15,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-source ../lib/ini.tcl
+source /usr/local/addons/lgtv/lib/ini.tcl
 
 namespace eval lgtv {
 	variable ini_file "../etc/lgtv.conf"
--- addon/www/rest.cgi
+++ addon/www/rest.cgi
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-source ../lib/lgtv.tcl
+source /usr/local/addons/lgtv/lib/lgtv.tcl
 
 # env:
 #  CONTENT_LENGTH
